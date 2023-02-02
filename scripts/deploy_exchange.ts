cf13495d07c68fa2672f3ce31a967847b93b65f4
ngduythao2805 <ngduythao2805@gmail.com>
2023-02-02 14:41:20 +0700

    build: exchange testnet

---
+++ scripts/deploy_exchange.ts
@@ -0,0 +1,17 @@
+import { Contract, ContractFactory } from "ethers";
+import { ethers, upgrades } from "hardhat";
+
+async function main(): Promise<void> {
+  const Logic: ContractFactory = await ethers.getContractFactory("StarExchange");
+  const logic: Contract = await upgrades.deployProxy(Logic, ["Exchange", "1"], { kind: "uups", initializer: "initialize" });
+  await logic.deployed();
+  console.log("Logic Proxy Contract deployed to : ", logic.address);
+  console.log("Logic Contract implementation address is : ", await upgrades.erc1967.getImplementationAddress(logic.address));
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error: Error) => {
+    console.error(error);
+    process.exit(1);
+  });
